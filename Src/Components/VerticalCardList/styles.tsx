ff5c0d851d895040f831f9c822bb212c66668b89
julio valente <julio.costa@ilia.digital>
2022-09-12 19:32:53 -0300

    feature/create-screen-weather

---
+++ Src/Components/VerticalCardList/styles.tsx
@@ -0,0 +1,42 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  top: 17%;
+  width: 100%;
+  height: 40%;
+  max-height: 40%;
+  background-color: ${({ theme }) => theme.colors.blackTransparence};
+  border-radius: 20px;
+`;
+
+export const List = styled.FlatList`
+  flex: 1;
+`;
+
+export const Row = styled.View`
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+export const ListTitle = styled.Text`
+  color: ${({ theme }) => theme.colors.white};
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: 20px;
+  padding: 6%;
+`;
+
+export const ListSubtitle = styled.Text`
+  color: #fff;
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: 20px;
+  width: 40%;
+  padding: 2%;
+  font-weight: 500;
+`;
+
+export const ListSubtitleSecondary = styled.Text`
+  color: ${({ theme }) => theme.colors.white};
+  font-size: 20px;
+  font-weight: 500;
+`;
--- Src/Routes/index.tsx
+++ Src/Routes/index.tsx
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { HomeScreen } from "../Pages/HomeScreen";
+import { ThemeProvider } from "styled-components/native";
 import { Provider } from "react-redux";
 import { store } from "../Store";
+import theme from "../Global/theme";
+import { useFonts, Poppins_400Regular } from "@expo-google-fonts/poppins";
+import { HomeScreen } from "../Pages/HomeScreen";
 
 const Stack = createNativeStackNavigator();
 
 export const Routes = () => {
+  let [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+  });
+  if (!fontsLoaded) {
+    return;
+  }
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-          <Stack.Screen name={"HomeScreen"} component={HomeScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <ThemeProvider theme={theme}>
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name={"HomeScreen"} component={HomeScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </ThemeProvider>
     </Provider>
   );
 };
